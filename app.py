43083c33ab0ba3e42b5fd22580ce912afa0a2f57
PAN-Yuguang <127849702+imaCollin@users.noreply.github.com>
2026-04-05 02:11:27 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-# from src.rag_chatbot.chatbot import build_app
-
-
-# demo = build_app()
-
-
-# if __name__ == "__main__":
-#     demo.launch()
-# import os
-
-# from src.rag_chatbot.chatbot import build_app
-
-
-# demo = build_app()
-
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 7860))
-#     demo.launch(server_name="0.0.0.0", server_port=port)
 import os
+
+import gradio_client.utils as gradio_client_utils
 
 from src.rag_chatbot.chatbot import build_app
 
+
+_original_get_type = gradio_client_utils.get_type
+
+
+def _safe_get_type(schema):
+    # Render hits a Gradio schema parsing edge case where a JSON schema node
+    # can be the boolean literal True/False instead of a dict.
+    if isinstance(schema, bool):
+        return {}
+    return _original_get_type(schema)
+
+
+gradio_client_utils.get_type = _safe_get_type
+
+
 demo = build_app()
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "7860"))
