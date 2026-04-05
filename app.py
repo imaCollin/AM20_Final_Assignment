5674adaece34657e993826c9f68905424382f8ac
PAN-Yuguang <127849702+imaCollin@users.noreply.github.com>
2026-04-05 01:40:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+# from src.rag_chatbot.chatbot import build_app
+
+
+# demo = build_app()
+
+
+# if __name__ == "__main__":
+#     demo.launch()
+import os
+
 from src.rag_chatbot.chatbot import build_app
 
 
@@ -5,4 +15,5 @@ demo = build_app()
 
 
 if __name__ == "__main__":
-    demo.launch()
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
